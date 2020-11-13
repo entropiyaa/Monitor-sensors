cbf9a34dab6da8da2e124a1ed1ecbe66e42a3c79
Tanya <tanyaz.228.67@gmail.com>
2020-11-14 01:28:40 +0300

    feature: gender in the registration form

--- frontend/src/app/pages/registration/components/registration.component.ts
+++ frontend/src/app/pages/registration/components/registration.component.ts
@@ -24,6 +24,7 @@ export class RegistrationComponent implements OnInit {
       passwordConfirmation: [''],
       firstName: [''],
       lastName: [''],
+      sex: [''],
       birthday: [''],
     });
   }
