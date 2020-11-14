752d490c262aeca17e1cc432366bdd642f006078
Tanya <tanyaz.228.67@gmail.com>
2020-11-14 04:11:42 +0300

    ui for profile page

--- frontend/src/app/pages/profile/components/profile.component.ts
+++ frontend/src/app/pages/profile/components/profile.component.ts
@@ -1,10 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  public examinationForm: FormGroup;
+  public visibilityNewExamination: boolean = false;
+  typesOfSensors: string[] = ['Blood pressure measurements', 'Skin temperature measurements',
+    'Skin moisture measurements', 'Measurements of electrical conductivity of the skin', 'Heart rate heart rate measurements'];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+  }
+
+  public createForm(): void {
+    this.examinationForm = this.fb.group({
+      type: [''],
+      type2: [''],
+    });
+    this.visibilityNewExamination = true;
+  }
+
+  public close(): void {
+    this.visibilityNewExamination = false;
+    this.examinationForm.reset();
+  }
 }
--- frontend/src/app/pages/profile/profile.module.ts
+++ frontend/src/app/pages/profile/profile.module.ts
@@ -1,5 +1,16 @@
 import { NgModule } from "@angular/core";
 import {ProfileComponent} from "./components/profile.component";
+import {TitleModule} from "../../modules/title/title.module";
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {CommonModule} from "@angular/common";
+import {BrowserModule} from "@angular/platform-browser";
+import {MatCardModule} from "@angular/material/card";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatSelectModule} from "@angular/material/select";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
@@ -7,7 +18,18 @@ import {ProfileComponent} from "./components/profile.component";
     ProfileComponent
   ],
   imports: [
-
+    TitleModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CommonModule,
+    BrowserModule,
+    MatCardModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatListModule,
   ],
   providers: [],
   exports: [ProfileComponent]
